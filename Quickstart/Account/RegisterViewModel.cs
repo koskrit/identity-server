b6a9f49683b3ef04bc26e9ac5e49a9a1584ab983
koskrit <koskritm@gmail.com>
2024-02-01 03:33:49 +0200

    Add Register Username Field & Show Validation Errors

--- Quickstart/Account/RegisterViewModel.cs
+++ Quickstart/Account/RegisterViewModel.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations;
 public class RegisterViewModel
 {
     [Required]
+    public string Username { get; set; }
+
+    [Required]
     [EmailAddress]
     public string Email { get; set; }
 
